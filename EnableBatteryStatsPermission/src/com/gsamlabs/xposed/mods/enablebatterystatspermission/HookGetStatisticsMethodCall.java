b7e1aa1d471d9ebf64947f26ba0bf4a75932faf8
gsamtan <george@gsamlabs.com>
2013-11-24 17:54:50 -0600

    Added hook for getService to allow legacy apps to still use the batteryinfo rather than batterystats service.

--- EnableBatteryStatsPermission/src/com/gsamlabs/xposed/mods/enablebatterystatspermission/HookGetStatisticsMethodCall.java
+++ EnableBatteryStatsPermission/src/com/gsamlabs/xposed/mods/enablebatterystatspermission/HookGetStatisticsMethodCall.java
@@ -20,6 +20,7 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 import java.lang.reflect.Field;
 
+import android.os.Build;
 import android.os.Parcel;
 import android.os.Parcelable;
 import de.robv.android.xposed.IXposedHookLoadPackage;
@@ -32,6 +33,13 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
  * getStatistics() method (accessed via IBatteryStats.getStatistics()) that does
  * not check for the BATTERY_STATS permission.  
  * 
+ * In addition, this will enable legacy apps which still use the 'batteryinfo' 
+ * rather than 'batterystats' service name to function as-is.
+ * 
+ * Other changes in KitKat such as how to access Network data (getTcpBytesReceived
+ * no longer exists for example) are not supported here.  So if your battery monitor 
+ * of choice still crashes - check the logcat and report it to the author.
+ * 
  * The purpose of this is for use in KitKat.  Since Google has decided to change 
  * the BATTERY_STATS permission to be signature|system 
  * (https://code.google.com/p/android/issues/detail?id=61975), thereby making it
@@ -42,7 +50,8 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class HookGetStatisticsMethodCall implements IXposedHookLoadPackage {
     
     private static final String BATTERY_STATS_SERVICE_NAME = "com.android.server.am.BatteryStatsService";
-    private static final boolean DEBUG = false;
+    private static final String SERVICE_MANAGER_NAME = "android.os.ServiceManager";
+    private static final boolean DEBUG = true;
     private static Field STATS_FIELD = null;
     // Get and modify the mStats field once - can do this statically on class load.
     static
@@ -68,6 +77,11 @@ public class HookGetStatisticsMethodCall implements IXposedHookLoadPackage {
         {
             return;
         }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT)
+        {
+            return;
+        }
+        
         // Check the package - only bother hooking for the system package.
         // We'll leave this commented out for now.  On my phone it's the
         // com.google.android.backup package that claims to be running this
@@ -87,7 +101,7 @@ public class HookGetStatisticsMethodCall implements IXposedHookLoadPackage {
         findAndHookMethod(BATTERY_STATS_SERVICE_NAME, lpparam.classLoader, "getStatistics", new XC_MethodHook() {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                if (!mIsLogged)
+                if (!mIsLogged && DEBUG)
                 {
                     XposedBridge.log("EnableBatteryStatsPermission hooking com.android.server.am.BatteryStatsService.getStatistics to remove BATTERY_STATS permission...");
                     mIsLogged = true;
@@ -108,7 +122,42 @@ public class HookGetStatisticsMethodCall implements IXposedHookLoadPackage {
                     XposedBridge.log("EnableBatteryStatsPermission hooking getStatistics <-");
                 }
             }
-        });        
+        });
+        
+        /**
+         * Also hook android.os.ServiceManager.getService and check to see whether they are
+         * passing in the 'old' service name.  This allows us to fully support a legacy app
+         * that hasn't been re-written for KitKat.
+         */
+        findAndHookMethod(SERVICE_MANAGER_NAME, lpparam.classLoader, "getService", String.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                if (!mIsLogged && DEBUG)
+                {
+                    XposedBridge.log("EnableBatteryStatsPermission hooking android.os.ServiceManager.getService to check for legacy use of batterystats service...");
+                    mIsLogged = true;
+                }
+                if (DEBUG)
+                {
+                    XposedBridge.log("EnableBatteryStatsPermission hooking getService ->");
+                    XposedBridge.log("EnableBatteryStatsPermission args: "+((param.args.length > 0) ? param.args[0] : "null"));
+                }
+                // If they sent in 'batterystats' as the service, we'll modify it to 'batteryinfo'
+                // and send it on down the line.
+                if ((param.args.length > 0) && "batteryinfo".equals(param.args[0]))
+                {
+                    if (DEBUG)
+                    {
+                        XposedBridge.log("EnableBatteryStatsPermission getService requested batteryinfo - changing to batterystats");
+                    }
+                    param.args[0] = "batterystats";
+                }     
+                if (DEBUG)
+                {
+                    XposedBridge.log("EnableBatteryStatsPermission hooking getService <-");
+                }
+            }
+        });     
     }
 
 }
